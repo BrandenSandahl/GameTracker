94e51d5764f141a8285d799ae7f12121708f7d0f
BrandenSandahl <branden157@gmail.com>
2016-02-29 19:55:38 -0500

    Finished all methods, added a search routine that will search for any part of a game title.

--- src/com/TheIronYard/Main.java
+++ src/com/TheIronYard/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
-
+    static boolean userSearch; //this is not very clever way to accomplish this. Anyone have better?
 
     public static void main(String[] args) throws SQLException {
         Spark.externalStaticFileLocation("public");
@@ -18,8 +18,7 @@ public class Main {
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
         Statement statement = conn.createStatement();
 
-
-        //create a new table if one does not exist
+        //create new tables if does not exist
         statement.execute("CREATE TABLE IF NOT EXISTS games (id IDENTITY, name VARCHAR, genre VARCHAR, platform VARCHAR, release_year INT, owner VARCHAR)");
         statement.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR)");
 
@@ -29,11 +28,17 @@ public class Main {
                     String loggedInName = getUserFromSession(request.session());
 
                     HashMap m = new HashMap();
-                    ArrayList<Game> games = new ArrayList<>();
+                    ArrayList<Game> games = new ArrayList<>(); //arraylist for displaying info from the DB
 
-                    if  (loggedInName == null) {
+                    if  (loggedInName == null) {  //if not logged in yet, lot in
                         return new ModelAndView(m, "login.html");
-                    } else {
+                    } else if (userSearch) { //if logged in and the user is trying to search
+                        String searchQuery = request.queryParams("searchQuery");
+                        games = selectGames(conn,loggedInName, searchQuery);
+                        userSearch = false; //reset option
+                        m.put("games", games);
+                        return new ModelAndView(m, "home.html");
+                    } else {  //if logged in display the users games
                            games = selectGames(conn, loggedInName); //local list from the DB
                         m.put("games", games);
                         return new ModelAndView(m, "home.html");
@@ -46,14 +51,22 @@ public class Main {
                 "/edit",
                 ((request2, response2) ->  {
                     int id = Integer.parseInt(request2.queryParams("id"));
-
-                   Game game = selectSingleGame(conn, id);
-
-
+                    Game game = selectSingleGame(conn, id);  //create a copy of the game to work with from the DB
                     return new ModelAndView(game, "edit.html");
 
                 }),
                 new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/search",
+                ((request3, response3) -> {
+                    String searchQuery = request3.queryParams("searchTitleInput");
+
+                    response3.redirect("/?searchQuery=" + searchQuery ); //just need to pull this in and push it back to the get route
+                    userSearch = true;
+                    return "";
+                })
         );
 
         Spark.post(
@@ -65,24 +78,19 @@ public class Main {
                     Session session = request.session();
 
                     //need to get my users from DB and see if there is already a user
-
                     PreparedStatement statement1 = conn.prepareStatement("SELECT name FROM users WHERE name = ?");
                     statement1.setString(1, loggedInName);
-                    //statement1.execute();
-
                     ResultSet resultSet = statement1.executeQuery();
 
-                    if (resultSet.next()) {
-                            session.attribute("userName", loggedInName);
+                    if (resultSet != null) {  //if we found anything in the DB
+                            session.attribute("userName", loggedInName);  //log in as that user
                     } else {
-                            //put into DB
+                            //put into DB and log in as the new user
                             PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?)");
                             preparedStatement.setString(1, loggedInName);
                             preparedStatement.execute();
                             session.attribute("userName", loggedInName);
-
                     }
-
                     response.redirect("/");
                     return "";
                 })
@@ -95,10 +103,10 @@ public class Main {
                     String loggedInAs = getUserFromSession(request.session()); //pulling a pointer to the user in the HashMap
 
                     if (loggedInAs == null) {
-                        //throw new Exception("User is not logged in");
                         Spark.halt(403); //throwing a spark error because user is not logged in.
                     }
 
+                    //get field from form
                     String gameName = request.queryParams("gameName");
                     String gameGenre = request.queryParams("gameGenre");
                     String gamePlatform = request.queryParams("gamePlatform");
@@ -110,11 +118,10 @@ public class Main {
 
                     //create a game
                     Game game = new Game(gameName, gameGenre, gamePlatform, gameYear, loggedInAs);
-                    //also add to DB
+                    // add to DB
                     createGame(conn, game);
 
                     response.redirect("/");
-
                     return "";
                 })
         );
@@ -124,9 +131,20 @@ public class Main {
                 ((request2, response2) -> {
                     int id = Integer.parseInt(request2.queryParams("id"));
 
-                    //PreparedStatement statement = conn.prepareStatement("UPDATE game SET name = ?, ")
+                    String gameName = request2.queryParams("gameName");
+                    String gameGenre = request2.queryParams("gameGenre");
+                    String gamePlatform = request2.queryParams("gamePlatform");
+                    int gameYear = Integer.parseInt(request2.queryParams("gameYear"));
 
+                    PreparedStatement statement1 = conn.prepareStatement("UPDATE games SET name = ?, genre = ?, platform = ?, release_year = ? WHERE id = ?");
+                    statement1.setString(1, gameName);
+                    statement1.setString(2, gameGenre);
+                    statement1.setString(3, gamePlatform);
+                    statement1.setInt(4, gameYear);
+                    statement1.setInt(5, id);
+                    statement1.execute();
 
+                    response2.redirect("/");
                     return "";
                 })
         );
@@ -137,7 +155,9 @@ public class Main {
                     int id = Integer.parseInt(request1.queryParams("id"));
 
                    //prepared statment here
-                    statement.execute("DELETE FROM games WHERE id=" + id +"");
+                    PreparedStatement statement1 = conn.prepareStatement("DELETE FROM games WHERE id = ?");
+                    statement1.setInt(1, id);
+                    statement1.execute();
 
                     response1.redirect("/");
                     return "";
@@ -172,10 +192,11 @@ public class Main {
 
     }
 
+
     static ArrayList<Game> selectGames(Connection conn, String loggedInName) throws SQLException {
         ArrayList<Game> games = new ArrayList<>();
 
-            PreparedStatement statement1 = conn.prepareStatement("SELECT * FROM games WHERE owner = ?");
+        PreparedStatement statement1 = conn.prepareStatement("SELECT * FROM games WHERE owner = ?");
             statement1.setString(1, loggedInName);
 
             ResultSet resultSet = statement1.executeQuery();
@@ -194,6 +215,34 @@ public class Main {
             }
 
         return games;
+    }
+
+    //OOOOVVVEEERRLLOOOOAAADDDD. yeah..it's an overloaded method.
+    static ArrayList<Game> selectGames(Connection conn, String loggedInName, String searchQuery) throws SQLException {
+        ArrayList<Game> games = new ArrayList<>();
+        PreparedStatement statement1 = conn.prepareStatement("SELECT * FROM games WHERE owner = ? AND LOWER(name) LIKE LOWER(?) ");
+        statement1.setString(1, loggedInName);
+   //   SEE: http://stackoverflow.com/questions/8247970/using-like-wildcard-in-prepared-statement
+        statement1.setString(2, "%" + searchQuery + "%");  //these %'s can not be in the prepare statement part for whatever reason
+
+        ResultSet resultSet = statement1.executeQuery();
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String genre = resultSet.getString("genre");
+            String platform = resultSet.getString("platform");
+            int releaseYear = resultSet.getInt("release_year");
+            String owner = resultSet.getString("owner");
+
+            Game game = new Game(name, genre, platform, releaseYear, owner); //create a game
+            game.id = id;  // set the id from the DB to the id of the game object
+            games.add(game); //add to temp list for display and interaction purposes.
+        }
+
+        return games;
+
+
     }
 
 
